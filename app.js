12c958984d9f0d66222bc4901eb77d1ba2a5eac7
stevensetiawan94 <steven.setiawan.94@gmail.com>
2020-03-02 18:50:39 +0700

    error handler

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 const router = require('./routes')
+const errorHandler = require ('./middleware/errorHandler')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/',router)
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`we are on port ${port}!`))
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -14,7 +14,7 @@ class todoController {
             .then(data => {
                 res.status(201).json(data)
             }).catch(err => {
-                res.status(400).json(err)
+                next(err)
             })
     }
 
@@ -33,11 +33,12 @@ class todoController {
                 id: req.params.id
             }
         }
+
         Todo.findOne(id)
             .then(data => {
                 res.status(200).json(data)
             }).catch(err => {
-                res.status(404).json(err)
+                next(err)
             })
     }
 
@@ -47,6 +48,7 @@ class todoController {
                 id: req.params.id
             }
         }
+       
         let dataResult = null
         Todo.findOne(id)
             .then(data => {
@@ -54,10 +56,16 @@ class todoController {
                 return Todo.destroy(id)        
             })
             .then(data =>{
+                if(data < 1){
+                    throw{
+                        code: 404,
+                        msg: `Data tidak ditemukan`
+                    }
+                }
                 res.status(200).json(dataResult)
             })
             .catch(err => {
-                res.status(400).json(err)
+                next(err)
             })
     }
 
@@ -74,15 +82,20 @@ class todoController {
             due_date: req.body.due_date
         }
 
-        let dataResult = null
-
         Todo.update(obj,id)
         .then(data=>{
+            if(data < 1){
+                throw{
+                    code:404,
+                    msg:`data tidak ditemukan`
+                }
+            }
             return Todo.findOne(id)
         }).then(data =>{
+               
              res.status(200).json(data)   
         }).catch(err => {  
-            res.status(400).json(err)
+            next(err)
         })
     }
 
