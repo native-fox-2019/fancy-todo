a1b9cfcee0c7aba0409699de9bad46e9b2dfa357
Singgit Bramantha <bambadom@gmail.com>
2020-03-04 01:13:48 +0700

    nambah api calendarific

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,6 @@
+const axios = require('axios').default
 const { todo } = require('../models')
+require('dotenv').config()
 
 class TodoController {
     static getTodo = (req, res) => {
@@ -37,10 +39,22 @@ class TodoController {
             description: req.body.description,
             status: req.body.status,
             due_date: req.body.due_date,
-            userId : req.userData.id
+            userId: req.userData.id
         }
         todo.create(obj)
             .then(data => {
+                // const sgMail = require('@sendgrid/mail');
+                // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+                // let message = 
+                // `<p>berhasil</p>`
+                // const msg = {
+                //     to: 'sacadss@gmail.com',
+                //     from: 'bambadom@gmail.com',
+                //     subject: `berhasil membuat task ${obj.title}`,
+                //     text: `${obj.description}`,
+                //     html: message,
+                // };
+                // sgMail.send(msg);
                 res.status(201).json(data)
             })
             .catch(err => {
@@ -105,6 +119,20 @@ class TodoController {
             })
             .then(data => {
                 res.status(200).json(temp)
+            })
+            .catch(err => {
+                res.status(500).json(err)
+            })
+    }
+
+    static holidays = (req, res) => {
+
+        axios({
+            method: 'get',
+            url: `https://calendarific.com/api/v2/holidays?&api_key=${process.env.CALENDARIFIC_API_KEY}&country=ID&year=2020`
+        })
+            .then(result => {
+                res.status(201).json(result.data.response.holidays)
             })
             .catch(err => {
                 res.status(500).json(err)
--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -3,11 +3,11 @@ const TodoController = require('../controllers/todoController')
 const authenticationUser = require('../middlewares/authentication')
 const authorizationUser = require('../middlewares/authorization')
 
+router.get('/holidays',TodoController.holidays)
 router.post('/', authenticationUser, TodoController.addTodo)
 router.get('/', authenticationUser, TodoController.getTodo)
 router.get('/:id', authenticationUser, authorizationUser, TodoController.getTodoId)
 router.put('/:id', authenticationUser, authorizationUser, TodoController.editTodo)
 router.delete('/:id', authenticationUser, authorizationUser, TodoController.deleteTodo)
-
 
 module.exports = router
