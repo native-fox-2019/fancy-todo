88229364e90eb901c47749df056796f3f0f68587
Singgit Bramantha <bambadom@gmail.com>
2020-03-04 17:23:00 +0700

    buat login register jquery

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const app = express()
 const port = process.env.PORT || 3000
 const routes = require('./routes/todoRouter')
 const routerUser = require('./routes/userRouter')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/todos' , routes)
