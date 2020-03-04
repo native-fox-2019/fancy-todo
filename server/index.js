96e84a27f70a6c0418a54498e187e8d2fc9e203f
agusr150 <ignatiusagus150@gmail.com>
2020-03-04 15:06:49 +0700

    register with ajax

---
+++ server/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 3000
+const cors = require('cors')
+
+const TodoRouter = require('./routes/todoRouter')
+const UserRouter = require('./routes/userRouter')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+ 
+app.use('/', TodoRouter)
+app.use('/user', UserRouter)
+
+app.listen(port,()=>console.log(`listening on post ${port}`))
