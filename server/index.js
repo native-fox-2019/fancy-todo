80ef9e57b83fced84fd42384a680248ce09392ea
Ignatius Agus <ignatiusagus@Ignatiuss-MacBook-Pro.local>
2020-03-05 00:51:07 +0700

    lanjut

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 const TodoRouter = require('./routes/todoRouter')
 const UserRouter = require('./routes/userRouter')
+const ApiRouter = require('./routes/apiRouter')
 
 app.use(cors())
 app.use(express.json())
@@ -13,5 +14,6 @@ app.use(express.urlencoded({extended: false}))
  
 app.use('/', TodoRouter)
 app.use('/user', UserRouter)
+app.use('/api', ApiRouter)
 
 app.listen(port,()=>console.log(`listening on post ${port}`))
--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -6,12 +6,12 @@ const axios = require('axios')
 router.get('/current_weather_data', function (req, res){
     axios({
         method: 'get',
-        url: 'https://community-open-weather-map.p.rapidapi.com/weather',
-        headers: {
-            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-            "x-rapidapi-key": "75d451094amsh903243283455443p16f6d8jsnf5e7688c9342"
-        }
+        url: 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,Indonesia&APPID='+process.env.API_KEY,
     })
+    .then(function (response) {
+        console.log(response.data)
+        res.send(response.data)
+      })
 })
 
 
