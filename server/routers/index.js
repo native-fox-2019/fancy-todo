38d5e8fdbea6faade5824264fcb8a5c46ca7e22e
agunggst <rqz.agastya@gmail.com>
2020-03-06 21:36:32 +0700

    3rd party api

--- server/routers/index.js
+++ server/routers/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 
 router.use('/todos', require('./todoRouter'))
 router.use('/users', require('./userRouter'))
+router.use('/seeHolidays', require('./holidayRouter'))
 
 module.exports = router
