513b2fd320a6e7143353fd8f25be98f372344c35
Singgit Bramantha <bambadom@gmail.com>
2020-03-09 16:51:08 +0700

    pre heroku deployment

--- server/config/config.js
+++ server/config/config.js
@@ -6,5 +6,12 @@ module.exports = {
     "database": "fancy_todo",
     "host": "127.0.0.1",
     "dialect": "postgres"
+  },
+  "production": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": "fancy_todo",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
   }
 }
