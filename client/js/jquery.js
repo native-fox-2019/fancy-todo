87cab7050ee8c9b26c72bd2456469992fe35b695
marcel <marcell.maruli021@gmail.com>
2020-03-09 17:04:15 +0700

    fixing bug

--- client/js/jquery.js
+++ client/js/jquery.js
@@ -68,6 +68,7 @@ function register() {
 }
 
 function editOne(id) {
+  $('#editForm').empty()
   $('#editPage').show()
   $.ajax({
     url: url + '/todos/' + id,
@@ -332,6 +333,9 @@ function addData() {
     },
   })
     .done((data) => {
+      $("#titleAdd").val("")
+      $("#descriptionAdd").val("")
+      $("#due_dateAdd").val("")
       Swal.fire({
         title: 'SUCCESS',
         text: "You added one task!!",
@@ -409,18 +413,15 @@ $('#showAddTodo').on('click', function (e) {
 })
 
 $('#addForm').on('submit', function (e) {
-  // e.preventDefault()
+  e.preventDefault()
   addData()
+  $('#addPage').hide()
   getData()
+  $('#todosTable').show()
+  $("#showTodo").addClass("active");
+  $("#showAddTodo").removeClass("active");
 })
 
-// function onSignIn(googleUser) {
-//   var profile = googleUser.getBasicProfile();
-//   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-//   console.log('Name: ' + profile.getName());
-//   console.log('Image URL: ' + profile.getImageUrl());
-//   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-// }
 
 function onSignIn(googleUser) {
   var id_token = googleUser.getAuthResponse().id_token;
